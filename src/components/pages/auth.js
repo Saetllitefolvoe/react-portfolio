c3e843f30135e7aa7b5540cff1f121f0c071fd73
Saetllitefolvoe <milo.tonon@gmail.com>
2026-06-16 09:46:36 +0200

    Fix image filename case sensitivity 2

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -3,39 +3,39 @@ import Login from "../auth/login";
 import loginImg from "../../../static/assets/images/auth/img_2064.jpg";
 
 export default class Auth extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
-        this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
-    }
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
+  }
 
-    handleSuccessfulAuth() {
-        this.props.handleSuccessfulLogin();
-        this.props.history.push("/");
-    }
+  handleSuccessfulAuth() {
+    this.props.handleSuccessfulLogin();
+    this.props.history.push("/");
+  }
 
-    handleUnsuccessfulAuth() {
-        this.props.handleUnsuccessfulLogin();
-    }
+  handleUnsuccessfulAuth() {
+    this.props.handleUnsuccessfulLogin();
+  }
 
-    render() {
-        return (
-            <div className="auth-page-wrapper">
-            <div className="left-column" 
-                style={{
-                    backgroundImage: `url(${loginImg})`
-                }}
-            />
-                
-            <div className="right-column">
-            <Login
-                handleSuccessfulAuth={this.handleSuccessfulAuth}
-                handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
-            />
-            </div>
-            </div>
-        );        
-    }
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
+
+        <div className="right-column">
+          <Login
+            handleSuccessfulAuth={this.handleSuccessfulAuth}
+            handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
